3c9602257af1f4eeeccfaedf8de0a9ea720dac0a
ZichengYan <464554492@qq.com>
2016-11-19 09:25:55 +0800

    修改为全局命令行工具

--- 002递归判断文件_文件夹.js
+++ 002递归判断文件_文件夹.js
@@ -1,26 +1,24 @@
-var fs = require("fs");
-
+const fs = require("fs");
+const pathO = require("path");
+var p = "../";
+var obj = {};
 //获取路径中的东西
 function loopAcc(path) {
+
     fs.readdir(path, (err, files) => {
-        console.log(files);
+        //console.log(files);
         files.forEach(function(val, index) {
-            //console.log(typeof val)
-            //console.log(val);
             var pat = path + "/" + val;
             fs.stat(pat, (err, stats) => {
-                //console.log(pat);
-                //console.log(stats);
                 if (err) throw err;
                 if (stats.isDirectory()) {
-                    console.log("文件夹");
+                   //此处逻辑不清晰
+                    //console.log("文件夹");
                     loopAcc(path + "/" + val);
                 } else if (stats.isFile()) {
-                    //console.log(pat + "文档");
-                    console.log(val);
-                    //read(pat);
+                    //console.log(val);
                 }
-            }); 
+            });
         });
     });
 }
@@ -30,10 +28,20 @@ function read(path) {
     fs.access(path, (err) => {
         if (!err) {
             fs.readFile(path, "utf8", (err, data) => {
-                console.log(data);
+                //console.log(data);
             });
         }
     });
 }
+//判断路径是否为文件夹
+function judgeFile(path) {
+    var stats = fs.statSync(path);
+    if (stats.isDirectory()) {
+        var baseName = pathO.basename(path);
+        obj[baseName] = { type: 0, baseName: {} };
+        loopAcc(path);
+    } else if (stats.isFile()) {
 
-loopAcc("./path");
+    }
+}
+judgeFile(p);
